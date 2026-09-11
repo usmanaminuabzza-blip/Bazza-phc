65584a11cda6c07f5431eb60e689d7dbe5eb30b4
usmanaminuabzza-blip <usmanaminuabzza@gmail.com>
2026-09-11 17:53:03 +0100

    Add initial App component for hospital management system

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bazza Primary Health Care Sokoto</h1>
+      <p>Hospital Management System</p>
+    </div>
+  );
+}
+
+export default App;
